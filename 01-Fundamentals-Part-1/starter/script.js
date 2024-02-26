c552afd4a2f7f00553056b8b925dbb48d31471a3
Ryan King <161281690+Ryan-King-Dev@users.noreply.github.com>
2024-02-25 22:05:26 -0800

    uploaded files

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,71 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+let country = "USA";
+let continent = "North America";
+let population = "335,893,238";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+true;
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let country = "USA";
+let continent = "North America";
+let population = 335893238;
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+sdfdsfdsfds
