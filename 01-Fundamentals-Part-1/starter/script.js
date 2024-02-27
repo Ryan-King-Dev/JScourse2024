c5be557e775ed1a5d2925fa51c0c2b43cb26d5b0
Ryan King <161281690+Ryan-King-Dev@users.noreply.github.com>
2024-02-26 22:18:21 -0800

    progressed in course

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,7 +33,6 @@ let population = "335,893,238";
 console.log(country);
 console.log(continent);
 console.log(population);
-*/
 
 true;
 let javascriptIsFun = true;
@@ -67,5 +66,69 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+*/
 
-sdfdsfdsfds
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+// // const job;
+
+// var job = 'programmer';
+// job = ' teacher';
+
+// lastName = 'Schmedtmann';
+// console.log(lastName);
+
+// const language = 'english';
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+// console.log(`Hi, my name is ${firstName} ${lastName}.`);
+
+// Assignment Operators
+// let x = 10 + 5;  // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1 = 101
+// x--; // x = x - 1 = 100
+// x--; // x = x - 1 = 99
+// console.log(x);
+
+// Comarison Operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2018);
+
+// let usaPopulation = 335893238;
+// console.log(usaPopulation / 2);
+// usaPopulation++;
+// console.log(usaPopulation);
+// const finlandPopulation = 6000000;
+// const populationComparison = usaPopulation > finlandPopulation;
+// console.log(populationComparison);
+// const avgPopulation = 33000000;
+// const avgComparison = usaPopulation > avgPopulation;
+// console.log(avgComparison);
+// const description = `USA has a population of ${usaPopulation}, and Finland has a population of ${finlandPopulation}.`
+// console.log(description);
+// console.log(25 - 10 - 5);
+
+// let x, y;
+// x = y = 25 - 10 - 5;  // x = y = 10, y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
